1f0135d1a06e31f338ad2e1a4421a0055758eaa5
garrett <garrettchollett@yahoo.com>
2022-11-03 21:26:24 -0500

    Added Comments

--- solver.hs
+++ solver.hs
@@ -15,7 +15,7 @@ type Box = (Point,Player)
 type Move = (Line,Player)
 -- Represents the game state as 
 -- include the boxes that have been made, the lines, what turn it is, and whose turn it is
-type Board = ([Box],[Line],Int,Player) 
+type Board = ([Point],[Box],[Line],Int,Player) 
 -------------------------------------------------------------------------------------------------
 --                             PRETTY SHOW FOR DOTS AND BOXES
 -------------------------------------------------------------------------------------------------
@@ -34,25 +34,29 @@ instance Show Player where
 -------------------------------------------------------------------------------------------------
 --                             FUNCTIONS FOR DOTS AND BOXES
 -------------------------------------------------------------------------------------------------
+-- constructs the starting board by taking an int which represents the sqrt of the number of dots
+-- inputing 3 would make a 3x3 board
 makeBoard :: Int -> Board
 makeBoard = undefined
-
-makeMove :: Point -> Point -> Board -> Maybe Line
-makeMove = undefined
-
+-- checks if the move is legal, aka all points are on the board, checks if the points are one apart
+-- in one of the allowed directions, and lastly checks if the line is already on the board
 isMoveLegal :: Point -> Point -> Board -> Boolean
 isMoveLegal = undefined
-
+-- if the move is legal, it returns a line that can be played, else, it returns nothing
+makeMove :: Point -> Point -> Board -> Maybe Line
+makeMove = undefined
+-- takes in a line and adds it to the board
+-- checks if the line can form a new box using "canMakeBox"
+-- if it can, it makes a box and adds it to the list as well
+-- it changes the player if a box wasn't made and keeps the player the same if not
 updateBoard :: Board -> Line -> Board
 updateBoard = undefined
-
-winnerCheck :: Board -> Player
+-- checks if someone one the game, will return nothing if noone has one yet
+winnerCheck :: Board -> Maybe Player
 winnerCheck = undefined
-
+-- checks if a box can be made out of the given list of lines
 canMakeBox :: [Line] -> Boolean
 canMakeBox = undefined
-
+-- makes a box out of the given lines and the player that made it
 makeBox :: [Line] -> Player -> Box
 makeBox = undefined
-
-
