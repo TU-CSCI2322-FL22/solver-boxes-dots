7027dd951d69178b321b275133740634a80e528d
garrett <garrettchollett@yahoo.com>
2022-11-18 11:58:32 -0600

    added comments and got the fogarty touch

--- Solver.hs
+++ Solver.hs
@@ -7,28 +7,31 @@ import GameState
 import Data.Maybe
 import Data.List
 
-whoWillWin :: Board -> Win
-whoWillWin board@(_,_,_,_,player) = case checkWin board of
-    Just (Winner x) -> Winner x
-    Just Tie -> Tie
-    Nothing
-        | (Winner player) `elem` vegeta -> Winner player
-        | Tie `elem` vegeta -> Tie
-        | otherwise -> Winner (negPlayer player)
-        where vegeta =  map whoWillWin $ catMaybes $ map (updateBoard board) (validMoves board)
 
+-- checks who will win/tie from the given board state
+whoWillWin :: Board -> Win
+whoWillWin board@(_,_,_,_,player) =
+    case checkWin board of
+         Just (Winner x) -> Winner x
+         Just Tie -> Tie
+         Nothing
+            | (Winner player) `elem` vegeta -> Winner player
+            | Tie `elem` vegeta -> Tie
+            | otherwise -> Winner (negPlayer player)
+            where vegeta =  map whoWillWin $ catMaybes $ map (updateBoard board) (validMoves board)
+
+-- gives the best move for the player to make based on which move will force a win/tie
 bestMove :: Board -> Maybe Move
 bestMove (_,_,[],_,_) = Nothing
-bestMove board@(_,_,_,_,player) 
-    | winnerPlayer /= Nothing = winnerPlayer
-    | tier /= Nothing = tier
-    | otherwise = Just ku
-    where winnerPlayer = (Winner player) `lookup` (gohan)
-          tier = Tie `lookup` gohan
-          gohan@((go,ku):chan) = catMaybes $ map krillin (validMoves board)
+bestMove board@(_,_,_,_,player) =
+    case ((Winner player) `lookup` gohan, Tie `lookup` gohan, gohan) of
+        (Just wm, _, _) -> Just wm
+        (Nothing, Just tm, _) -> Just tm
+        (Nothing, Nothing, (wn,mv):_) -> Just mv
+    where gohan = catMaybes $ map krillin (validMoves board)
           krillin :: Move -> Maybe (Win,Move)
           krillin move = case updateBoard board move of
-            Nothing -> Nothing
-            Just x -> Just (whoWillWin x, move)
+                            Nothing -> Nothing
+                            Just x -> Just (whoWillWin x, move)
 
 -- "He ain't beating Goku tho" - Matt
