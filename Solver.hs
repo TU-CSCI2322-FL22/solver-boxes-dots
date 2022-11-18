87932b1816d942cc3a7549666fbf19205c97c1c8
mmendio2 <mmendio2@trinity.edu>
2022-11-17 22:10:44 -0600

    Finished ugly version of Best-Move
    Co-authored-by: Gchollett <Gchollett@users.noreply.github.com>
    Co-authored-by: mtierrab <mtierrab@users.noreply.github.com>

--- Solver.hs
+++ Solver.hs
@@ -5,17 +5,29 @@ module Solver
 
 import GameState
 import Data.Maybe
+import Data.List
 
 whoWillWin :: Board -> Win
 whoWillWin board@(_,_,_,_,player) = case checkWin board of
     Just (Winner x) -> Winner x
     Just Tie -> Tie
     Nothing
-        | (Winner player) `elem` it -> Winner player
-        | Tie `elem` it -> Tie
+        | (Winner player) `elem` vegeta -> Winner player
+        | Tie `elem` vegeta -> Tie
         | otherwise -> Winner (negPlayer player)
-        where it =  map whoWillWin $ catMaybes $ map (updateBoard board) (validMoves board)
+        where vegeta =  map whoWillWin $ catMaybes $ map (updateBoard board) (validMoves board)
 
 bestMove :: Board -> Move
-bestMove board@(_,_,_,_,player) = undefined
+bestMove board@(_,_,_,_,player) 
+    | winnerPlayer /= Nothing = fromJust winnerPlayer
+    | tier /= Nothing = fromJust tier
+    | otherwise = ku
+    where winnerPlayer = (Winner player) `lookup` (gohan)
+          tier = Tie `lookup` gohan
+          gohan@((go,ku):chan) = catMaybes $ map krillin (validMoves board)
+          krillin :: Move -> Maybe (Win,Move)
+          krillin move = case updateBoard board move of
+            Nothing -> Nothing
+            Just x -> Just (whoWillWin x, move)
+
 -- "He ain't beating Goku tho" - Matt
--- Tests.hs
+++ Tests.hs
@@ -4,9 +4,13 @@ import Solver
 Just startBoard = makeBoard 3
 startPrint = putStr (prettyShowBoard startBoard)
 boardWithBox = ([((1,1), Blue)],[((1,1),Dwn),((1,1),Rght),((1,2),Dwn),((2,1),Rght)],[((1,2),Rght), ((1,3),Dwn),((2,1),Dwn),((2,2),Dwn),((2,2),Rght),((2,3),Dwn),((3,1),Rght),((3,2),Rght)],3,Blue)
+
 wholeBoard3 = ([((1,1),Blue),((1,2),Blue),((2,1),Blue),((2,2),Red)], (legalMoves 3), [], 3, Blue)
 wholeBoard4 = ([((1,1),Blue),((1,2),Blue),((2,1),Blue),((2,2),Red),((1,3),Red),((2,3),Blue),((3,1),Blue),((3,2),Red),((3,3),Blue)], (legalMoves 4), [], 4, Blue)
 Just testBoard = updateBoard boardWithBox ((1,3),Dwn)
+
+
+
 printWholeBoard3 = putGame wholeBoard3
 printTest = putGame testBoard
 it = read "([((1,1), Blue)],[((1,1),Dwn),((1,1),Rght),((1,2),Dwn),((2,1),Rght)],[((1,2),Rght), ((1,3),Dwn),((2,1),Dwn),((2,2),Dwn),((2,2),Rght),((2,3),Dwn),((3,1),Rght),((3,2),Rght)],3,Blue)" :: Board
