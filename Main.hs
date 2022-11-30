244dfb37d8a2bce414e31420227ee83c76e31dda
garrett <garrettchollett@yahoo.com>
2022-11-30 15:51:47 -0600

    erased comments

--- Main.hs
+++ Main.hs
@@ -11,26 +11,3 @@ main = do
   game <- loadGame fname
   putWinner game
   return ()
-  
-
---   putStr "Move Format: (x1,y1) (x2,y2)"
---   let move = getMove game
---   return ()
-
--- getMove :: Board -> Move
--- getMove board = do
---     input <- prompt "Input move"
---     let (p1:p2:ps) = map read (words input) :: [Point]
---     let move = makeMove p1 p2 board
---     case move of
---         Nothing -> getMove board
---         Just x -> x
-        
-
-
--- prompt :: String -> IO String
--- prompt question = do
---   putStr $ question ++ ": "
---   hFlush stdout
---   response <- getLine
---   return response
