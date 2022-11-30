e55c53795ff8a9d2f310134620b401583d35b460
garrett <garrettchollett@yahoo.com>
2022-11-30 15:41:11 -0600

    did some file stuff

--- Main.hs
+++ Main.hs
@@ -7,7 +7,7 @@ import System.Environment
 main :: IO ()
 main = do
   args <- getArgs
-  let fname = if null args then "gamestate.txt" else head args
+  let fname = if null args then "TestFiles/gamestate.txt" else head args
   game <- loadGame fname
   putWinner game
   return ()
--- Tests.hs
+++ Tests.hs
@@ -9,6 +9,7 @@ boardWithBox = ([((1,1), Blue)],[((1,1),Dwn),((1,1),Rght),((1,2),Dwn),((2,1),Rgh
 wholeBoard3 = ([((1,1),Blue),((1,2),Blue),((2,1),Blue),((2,2),Red)], (legalMoves 3), [], 3, Blue)
 wholeBoard4 = ([((1,1),Blue),((1,2),Blue),((2,1),Blue),((2,2),Red),((1,3),Red),((2,3),Blue),((3,1),Blue),((3,2),Red),((3,3),Blue)], (legalMoves 4), [], 4, Blue)
 Just testBoard = updateBoard boardWithBox ((1,3),Dwn)
+Just testBoard2 = updateBoard testBoard ((3,1),Rght)
 
 
 
